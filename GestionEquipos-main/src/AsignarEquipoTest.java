80d7e2a3a3f168cc5c4590feb9399aaef9762818
AlvaroCesur <alvaro.s11457@cesurformacion.com>
2022-01-24 12:30:16 +0100

    CREADO

---
+++ GestionEquipos-main/src/AsignarEquipoTest.java
@@ -0,0 +1,22 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AsignarEquipoTest {
+
+	@Test
+	void testAsignarEquipo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetJugador() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetJugador() {
+		fail("Not yet implemented");
+	}
+
+}
