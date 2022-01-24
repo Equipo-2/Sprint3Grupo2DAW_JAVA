b883a12c22c2d364fe9baf5f6bfa40b188f65ffd
Lizzy <93984049+ANLizzy@users.noreply.github.com>
2022-01-24 12:37:51 +0100

    Equipo

---
+++ GestionEquipos-main/src/EquipoTest.java
@@ -0,0 +1,123 @@
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * 
+ */
+
+/**
+ * @author Alicia Ruiznavarro
+ *
+ */
+class EquipoTest {
+	
+//###################################      NOMBRE      ###################################
+
+	//Guardar jugador con 0 caracteres
+	@Test void testSetNombreJugadorInvalidoVacio() {
+		Jugador jugador = new Jugador();
+		String nombreJugador = "";
+		jugador.setNombreJugador(nombreJugador);
+		assertEquals(null, jugador.getNombreJugador());
+	}
+
+	//Guardar jugador con menos de 4 caracteres
+	@Test void testSetNombreJugadorInvalidoMinimoCaracteres() {
+		Jugador jugador = new Jugador();
+		String nombreJugador = "Ana";
+		jugador.setNombreJugador(nombreJugador);
+		assertEquals(null,jugador.getNombreJugador());
+	}
+
+	//Guardar jugador con 4 caracteres
+	@Test void testSetNombreJugadorValidoMinimoCaracteres() {
+		Jugador jugador = new Jugador();
+		String nombreJugador = "Juan";
+		jugador.setNombreJugador(nombreJugador);
+		assertEquals(nombreJugador,jugador.getNombreJugador());
+	}
+
+	//Guardar jugador con más de 4 caracteres
+	@Test void testSetNombreJugadorValido() {
+		Jugador jugador = new Jugador();
+		String nombreJugador = "Mortimer";
+		jugador.setNombreJugador(nombreJugador);
+		assertEquals(nombreJugador, jugador.getNombreJugador());
+	}
+
+	//Guardar jugador con menos de 20 caracteres
+	@Test void testSetNombreJugadorValido2() {
+		Jugador jugador = new Jugador();
+		String nombreJugador = "Pepe Domingo";
+		jugador.setNombreJugador(nombreJugador);
+		assertEquals(nombreJugador,jugador.getNombreJugador());
+	}
+
+	//Guardar jugador con 20 caracteres
+	@Test void testSetNombreJugadorValidoMaximoCaracteres() {
+		Jugador jugador = new Jugador();
+		String nombreJugador = "Pepe Domingo CastaÃ±o";
+		jugador.setNombreJugador(nombreJugador);
+		assertEquals(nombreJugador,jugador.getNombreJugador());
+	}
+
+	//Guardar jugador con más de 20 caracteres
+	@Test void testSetNombreJugadorInvalidoMaximoCaracteres() {
+		Jugador jugador = new Jugador();
+		String nombreJugador = "Pepe Domingo CastaÃ±o ";
+		jugador.setNombreJugador(nombreJugador);
+		assertEquals(null,jugador.getNombreJugador());
+	}
+	
+//###################################      RANKING      ###################################
+	
+	//Guardar edad con INT = 'char'
+	//El valor numérico de 'M' es aceptado como edad >= 18, terrible error
+	@Test
+	void testSetEdadInvalidoChar () {
+		Jugador jugador = new Jugador();
+		int edadJugador = 'M';
+		jugador.setEdad(edadJugador);
+//		assertEquals(null,jugador.getEdad());
+	}
+	
+	//Guardar edad con INT = 0
+	@Test
+	void testSetEdadInvalidoMinimo () {
+		Jugador jugador = new Jugador();
+		int edadJugador = 0;
+		jugador.setEdad(edadJugador);
+//		assertEquals(null,jugador.getEdad());
+	}
+	
+	//Guardar edad con INT < 18
+	@Test
+	void testSetEdadInvalidoMaximo () {
+		Jugador jugador = new Jugador();
+		int edadJugador = 17;
+		jugador.setEdad(edadJugador);
+//		assertEquals(null,jugador.getEdad());
+	}
+	
+	//Guardar edad con INT = 18
+	@Test
+	void testSetEdadValidoMinimo () {
+		Jugador jugador = new Jugador();
+		int edadJugador = 18;
+		jugador.setEdad(edadJugador);
+		assertEquals(edadJugador,jugador.getEdad());
+	}
+	
+	//Guardar edad con INT > 18
+	@Test
+	void testSetEdadValido () {
+		Jugador jugador = new Jugador();
+		int edadJugador = 19;
+		jugador.setEdad(edadJugador);
+		assertEquals(edadJugador,jugador.getEdad());
+	}
+
+}
