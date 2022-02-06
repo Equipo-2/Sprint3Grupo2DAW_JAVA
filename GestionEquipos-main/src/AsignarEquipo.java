8e2d7baefd898dbd20a7ca1d4e44b9ba4e0967e7
AlejandroPascualMoreno <alejandro.s262089@cesurformacion.com>
2022-02-06 18:57:44 +0100

    equipojava

--- GestionEquipos-main/src/AsignarEquipo.java
+++ GestionEquipos-main/src/AsignarEquipo.java
@@ -1,33 +1,39 @@
 
 public class AsignarEquipo {
-      private Equipo equipo;
-      private Jugador jugador;
-      
-      
-      public AsignarEquipo() {
-    	  
-      }
-      
-      public void setEquipo(Equipo equipo) {
-  		//code here
-      }
-      
-      public void setJugador(Jugador jugador) {
+	private Equipo equipo;
+	private Jugador jugador;
 
-    	  if (jugador.getNombreJugador()!=null && jugador.getEdad()>18 && jugador.getEdad()<100 && jugador.getIdioma()!=null) {
-    		  this.jugador = jugador;
-    	  }else {
-    		  this.jugador = null;
-    	  }
-    	  
-      }
-      
-      public Equipo getEquipo(){
-          return equipo;
-      }
-      
-      public Jugador getJugador(){
-         return jugador;
-      }
-      
+
+	public AsignarEquipo() {
+
+	}
+
+	public void setEquipo(Equipo equipo) {
+		if(equipo.getNombreEquipo()!=null && equipo.getRanking()!=-1) {
+			this.equipo=equipo;
+		}else {
+			this.equipo=null;
+		}
+
+	}
+
+
+	public void setJugador(Jugador jugador) {
+
+		if (jugador.getNombreJugador()!=null && jugador.getEdad()>18 && jugador.getEdad()<100 && jugador.getIdioma()!=null) {
+			this.jugador = jugador;
+		}else {
+			this.jugador = null;
+		}
+
+	}
+
+	public Equipo getEquipo(){
+		return equipo;
+	}
+
+	public Jugador getJugador(){
+		return jugador;
+	}
+
 }
